3dece2750d4c5527ff9db2bc87da63b16b34cc14
leopan0529 <jiabinpan.leo@gmail.com>
2023-01-10 10:08:00 -0600

    DOM Exercise

--- JS challenge.js
+++ JS challenge.js
@@ -121,4 +121,69 @@ function  zipArrays(a,b){
 // },2000)
 
 
+// ================================= WARM UP
+
+// Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with only the username and password properties. Assume at least an array of one user object.
+//
+//
+    const users = [
+    {
+        firstName: 'Justin',
+        lastName: 'Reich',
+        dob: '1923-01-01',
+        username: 'jreich',
+        password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+    },
+    {
+        firstName: 'Sally',
+        lastName: 'Smith',
+        dob: '1935-03-11',
+        username: 'ssmith',
+        password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+    },
+    {
+        firstName: 'Fred',
+        lastName: 'Smith',
+        dob: '1999-01-21',
+        username: 'fsmith',
+        password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+    },
+]
+//
+// getUserCredentials(users) // returns...
+//
+// /*
+//
+// [
+//   {
+//     username: 'jreich',
+//     password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+//   },
+//   {
+//     username: 'ssmith',
+//     password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+//   },
+//   {
+//     username: 'fsmith',
+//     password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+//   }
+// ]
+//
+// */
+
+
+function getUserCredentials(users){
+
+    for(var i=0;i<users.length;i++){
+            delete users[i].firstName;
+            delete users[i].dob;
+            delete users[i].lastName
+
+    }
+    return users
+
+}
+
+
+console.log(getUserCredentials(users))
 
